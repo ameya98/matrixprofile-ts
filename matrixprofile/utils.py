2d784f39950e9bcae2eaefea289f6edcac3447c2
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-06-18 11:25:23 -0700

    Code formatting/spacing improvements.

--- matrixprofile/utils.py
+++ matrixprofile/utils.py
@@ -10,6 +10,7 @@ range = getattr(__builtins__, 'xrange', range)
 import numpy as np
 import numpy.fft as fft
 
+
 def zNormalize(ts, znorm_threshold=0):
     """
     Returns a z-normalized version of a time series.
@@ -30,6 +31,7 @@ def zNormalize(ts, znorm_threshold=0):
 
     return ts
 
+
 def zNormalizeEuclidian(tsA,tsB):
     """
     Returns the z-normalized Euclidian distance between two time series.
@@ -45,6 +47,7 @@ def zNormalizeEuclidian(tsA,tsB):
 
     return np.linalg.norm(zNormalize(tsA.astype("float64")) - zNormalize(tsB.astype("float64")))
 
+
 def movmeanstd(ts,m):
     """
     Calculate the mean and standard deviation within a moving window passing across a time series.
@@ -58,9 +61,9 @@ def movmeanstd(ts,m):
         raise ValueError("Query length must be longer than one")
 
     ts = ts.astype("float")
-    #Add zero to the beginning of the cumsum of ts
+    # Add zero to the beginning of the cumsum of ts
     s = np.insert(np.cumsum(ts),0,0)
-    #Add zero to the beginning of the cumsum of ts ** 2
+    # Add zero to the beginning of the cumsum of ts ** 2
     sSq = np.insert(np.cumsum(ts ** 2),0,0)
     segSum = s[m:] - s[:-m]
     segSumSq = sSq[m:] -sSq[:-m]
@@ -69,6 +72,7 @@ def movmeanstd(ts,m):
     movstd = np.sqrt(segSumSq / m - (segSum/m) ** 2)
 
     return [movmean,movstd]
+
 
 def movstd(ts,m):
     """
@@ -83,14 +87,15 @@ def movstd(ts,m):
         raise ValueError("Query length must be longer than one")
 
     ts = ts.astype("float")
-    #Add zero to the beginning of the cumsum of ts
+    # Add zero to the beginning of the cumsum of ts
     s = np.insert(np.cumsum(ts),0,0)
-    #Add zero to the beginning of the cumsum of ts ** 2
+    # Add zero to the beginning of the cumsum of ts ** 2
     sSq = np.insert(np.cumsum(ts ** 2),0,0)
     segSum = s[m:] - s[:-m]
     segSumSq = sSq[m:] -sSq[:-m]
 
     return np.sqrt(segSumSq / m - (segSum/m) ** 2)
+
 
 def slidingDotProduct(query,ts):
     """
@@ -105,33 +110,30 @@ def slidingDotProduct(query,ts):
     m = len(query)
     n = len(ts)
 
-
-    #If length is odd, zero-pad time time series
+    # If length is odd, zero-pad time time series
     ts_add = 0
     if n%2 ==1:
         ts = np.insert(ts,0,0)
         ts_add = 1
 
+    # If length is odd, zero-pad query
     q_add = 0
-    #If length is odd, zero-pad query
     if m%2 == 1:
         query = np.insert(query,0,0)
         q_add = 1
 
-    #This reverses the array
+    # This reverses the array
     query = query[::-1]
+    query = np.pad(query, (0, n-m+ts_add-q_add), 'constant')
 
-
-    query = np.pad(query,(0,n-m+ts_add-q_add),'constant')
-
-    #Determine trim length for dot product. Note that zero-padding of the query has no effect on array length, which is solely determined by the longest vector
+    # Determine trim length for dot product. Note that zero-padding of the query has no effect on array length, which is solely determined by the longest vector
     trim = m-1+ts_add
 
     dot_product = fft.irfft(fft.rfft(ts)*fft.rfft(query))
 
+    # Note that we only care about the dot product results from index m-1 onwards, as the first few values aren't true dot products (due to the way the FFT works for dot products)
+    return dot_product[trim:]
 
-    #Note that we only care about the dot product results from index m-1 onwards, as the first few values aren't true dot products (due to the way the FFT works for dot products)
-    return dot_product[trim :]
 
 def DotProductStomp(ts,m,dot_first,dot_prev,order):
     """
@@ -151,7 +153,7 @@ def DotProductStomp(ts,m,dot_first,dot_prev,order):
 
     dot += ts[order+m-1]*ts[m-1:l+m]-ts[order-1]*np.roll(ts[:l],1)
 
-    #Update the first value in the dot product array
+    # Update the first value in the dot product array
     dot[0] = dot_first[order]
 
     return dot
@@ -208,7 +210,7 @@ def massStomp(query,ts,dot_first,dot_prev,index,mean,std, std_noise=0):
     return res, dot
 
 
-def apply_av(mp,av=[1.0]):
+def apply_av(mp, av=[1.0]):
     """
     Applies an annotation vector to a Matrix Profile.
 
